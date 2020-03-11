38c698505d5c1cd95946dcf2791bf569d670351a
Oertlig <oertlig@oregonstate.edu>
2020-03-11 15:17:23 -0700

    start of project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <h1> Mars Weather </h1>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
