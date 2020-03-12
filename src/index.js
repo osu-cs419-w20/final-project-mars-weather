918ad8ea3a4cfc143646d9e794a0e002fbd5b3fc
yanyan2019 <leeya@oregonstate.edu>
2020-03-11 23:58:23 -0700

    add global css

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import {Global, css} from '@emotion/core';
 
+const globalStyles = css`
+     body{
+	margin: 0px;
+     }
+`;
 
 ReactDOM.render(
   <BrowserRouter>
+    <Global styles={globalStyles} />
     <App />
   </BrowserRouter>,
   document.getElementById('root')
