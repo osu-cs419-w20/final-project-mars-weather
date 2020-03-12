9e27302eb03db2ac1bc3b5681b0828d416314b4a
yanyan2019 <leeya@oregonstate.edu>
2020-03-11 23:18:59 -0700

    add css to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,35 @@
 import React from 'react';
-
+import styled from '@emotion/styled/macro';
 import {
          NavLink
 	} from 'react-router-dom';
 
+const BarStyled = styled.nav`
+   margin: 0px;
+   height: 45px;
+   
+   a{
+	text-decoration: none;
+	color: black;
+	padding: 11px;
+	font-size: 35px;
+    }
+
+    a.active{
+	background-color: lightgrey; 
+    }
+`;
 
 function NavBar(){
+  
   return (
      <div>
-	<nav>
+	<BarStyled>
 	   <NavLink exact to ="/">Home</NavLink>
  	   <NavLink to="/Weather">Mars Weather</NavLink>
            <NavLink to="/MarsRoverPhoto">Mars Rover Photo</NavLink>
            <NavLink to="/About">About</NavLink>
-	</nav>  
+	</BarStyled>  
      </div>
   );
 }
