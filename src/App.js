d81b3663e995ec2fd622c31ae7afc6358511f945
Oertlig <oertlig@oregonstate.edu>
2020-03-16 12:49:05 -0700

    passing data to RoverPhotos

--- src/App.js
+++ src/App.js
@@ -5,35 +5,42 @@ import Weather from './pages/Weather';
 import MarsRoverPhoto from './pages/MarsRoverPhoto';
 import NotFound from './pages/NotFound';
 import NavBar from './components/NavBar';
+import queryString from 'query-string';
 import { Switch,
-	 Route
-	 
+	 Route,
+	 useLocation
+
 	} from 'react-router-dom';
+
+function useQueryString() {
+  return queryString.parse(useLocation().search);
+}
 
 function App() {
   return (
     <div>
-	<NavBar />
-	<main>
-	  <Switch>
-	    <Route path="/MarsRoverPhoto">
-	  	<MarsRoverPhoto />
-	    </Route>
-	    <Route path="/Weather">
-	  	<Weather />
-	    </Route>
-	    <Route path="/About">
-		<About />
-	    </Route>
-	    <Route exact path="/">
-	  	<Home />
-	    </Route>
-	    <Route path="*">
-	  	<NotFound />
-	    </Route>
-	  </Switch>
-        </main>
-      
+			<NavBar />
+			<main>
+			  <Switch>
+
+					<Route path="/MarsRoverPhoto">
+			  		<MarsRoverPhoto date={useQueryString().earth_date}/>
+			    </Route>
+			    <Route path="/Weather">
+			  		<Weather />
+			    </Route>
+			    <Route path="/About">
+						<About />
+			    </Route>
+			    <Route exact path="/">
+			  		<Home />
+			    </Route>
+			    <Route path="*">
+			  		<NotFound />
+		 			</Route>
+
+			  </Switch>
+		  </main>
     </div>
   );
 }
