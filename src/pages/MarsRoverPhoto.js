d23b4c2bef849d2dd0dae6f3f6ba99520cec7817
Oertlig <oertlig@oregonstate.edu>
2020-03-16 12:50:32 -0700

    search for date and call to api with that earth date

--- src/pages/MarsRoverPhoto.js
+++ src/pages/MarsRoverPhoto.js
@@ -1,9 +1,171 @@
-import React from 'react';
+import React, {
+          useEffect,
+	        useState
+       } from 'react';
 
-function MarsRoverPhoto(){
+import fetch from 'isomorphic-unfetch';
+import { useHistory } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+const Input = styled.input`
+  border: 1px solid #ababab;
+  padding: 5px;
+  font-size: 18px;
+`;
+
+// const TextArea = Input.withComponent('textarea');
+
+const Button = styled.button`
+  background-color: #2b7bbe;
+  color: #fff;
+  border: 2px solid #2b7bbe;
+  border-radius: 3px;
+  font-size: 18px;
+  font-weight: 300;
+  padding: 5px 10px;
+  margin: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: #71b5ed;
+  }
+`;
+
+function RoverContent({ date }) {
+  const [ inputDate, setInputDate ] = useState(date || "");
+  const [ data, setData ] = useState([]);
+  const history = useHistory();
+  // const api = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2018-05-03&api_key=c872L3iRHOM9jlIPxbLTb0hgRk1q87Cs0jmDBcPh";
+
+
+  // const [ loading, setLoading ] = useState(false);
+
+
+  useEffect(() => {
+    if(date) {
+      if (date.match(/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]/i) || date==="") {
+        console.log("loading data");
+        const controller = new AbortController();
+
+        async function fetchRover() {
+          let responseData = {};
+          // setLoading(true);
+          try {
+            const response = await fetch(
+              `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=c872L3iRHOM9jlIPxbLTb0hgRk1q87Cs0jmDBcPh`,
+            { signal: controller.signal }
+            );
+            responseData = await response.json();
+          } catch (e) {
+            if (e instanceof DOMException) {
+                console.log("== HTTP request aborted");
+            } else {
+                console.log(e);
+            }
+          }
+
+            setData(responseData.photos);
+
+        }
+        fetchRover();
+        return () => {
+          controller.abort();
+        };
+      }
+      else{
+        alert("Not a valid date");
+        return;
+      }
+    }
+
+  }, [ date ]);
+
+  console.log(data);
+  // console.log("date: " + date);
+  if(data) {
+    if(data.length > 0) {
+      console.log("data");
+      return(
+         <div>
+           <form onSubmit={(e) => {
+             e.preventDefault();
+             history.push(`?earth_date=${inputDate}`);
+           }}>
+             <Input
+               value={inputDate}
+               onChange={e => setInputDate(e.target.value)}
+             />
+
+             <Button type="submit">Search Date</Button>
+           </form>
+
+           <div>
+             {data.map(rover => (
+               <div key={rover.id}>
+                 <img src={rover.img_src} alt="from the mars rover" />
+               </div>
+             ))}
+           </div>
+
+         </div>
+      );
+    }
+    else {
+      console.log("no data");
+      return (
+        <div>
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            history.push(`?earth_date=${inputDate}`);
+          }}>
+            <Input
+              value={inputDate}
+              onChange={e => setInputDate(e.target.value)}
+            />
+
+            <Button type="submit">Search Date</Button>
+          </form>
+
+          <div>
+            <h3> There are no photos for this date </h3>
+          </div>
+
+        </div>
+      );
+    } // end if
+
+  }
+  else {
+    console.log("no data");
+    return (
+      <div>
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          history.push(`?earth_date=${inputDate}`);
+        }}>
+          <Input
+            value={inputDate}
+            onChange={e => setInputDate(e.target.value)}
+          />
+
+          <Button type="submit">Search Date</Button>
+        </form>
+
+        <div>
+          <h3> Please enter a real date </h3>
+        </div>
+
+      </div>
+    );
+  }
+
+}
+
+function MarsRoverPhoto({date}){
+  // console.log("MarsRoverPhoto date:" + date);
   return (
     <div>
-       <h1>MarsRoverPhoto</h1>
+      <h1>MarsRoverPhoto</h1>
+      <RoverContent date={date}/>
     </div>
   );
 }
