62197076b230d5977a019c71cda84d0c17b56827
Oertlig <oertlig@oregonstate.edu>
2020-03-17 14:06:53 -0700

    updated code format

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import {Global, css} from '@emotion/core';
 import pic from "./pics/Mars6.jpg";
+
 const globalStyles = css`
+  body {
+  	margin: 0px;
+  	background-image:url(${pic});
+  	background-repeat: no-repeat;
+  	background-attachment: fixed;
+  	background-size: cover;
+  	color: black;
 
-     body{
-	margin: 0px;
-	background-image:url(${pic});
-	background-repeat: no-repeat;
-	background-attachment: fixed;
-	background-size: cover;
-	color: black;     
-	
-     }
+  }
 
-     .pageTitle{
-	text-align: center;
-     }
+  .pageTitle{
+    text-align: center;
+  }
 
 `;
 
